318aa4eb457f8bf740fdc5c996c0b3516ad52eab
inca <boris@okunskiy.name>
2026-07-10 19:04:48 +0300

    chore: add ws-proxy

--- src/main/backend.ts
+++ src/main/backend.ts
@@ -4,3 +4,4 @@ export * from '@flexent/jwt';
 export * from '@flexent/protocomm-http';
 export * from '@flexent/protocomm-ws';
 export * from '@flexent/stack-backend';
+export * from '@flexent/ws-proxy';
